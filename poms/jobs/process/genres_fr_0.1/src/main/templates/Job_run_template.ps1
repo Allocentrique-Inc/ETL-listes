d6cb82f0467dfdd20834aefe1b47062ccc57635a
Vincent de Grandpré <vincent@de-grandpre.quebec>
2019-06-27 16:09:21 -0400

    Extraction des listes de genres EN et FR depuis Wikipedia

---
+++ poms/jobs/process/genres_fr_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/json_simple-1.1.jar;../lib/log4j-1.2.17.jar;genres_fr_0_1.jar;' listes_iptoki.genres_fr_0_1.Genres_Fr  %*
